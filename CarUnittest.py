2c5eda04c95342af229535b8695bfe47017f6a10
C1N0 <cinnn0theprogrammer@gmail.com>
2024-04-13 16:43:38 +0300

    Added the tests for equivalence partitioning.

--- CarUnittest.py
+++ CarUnittest.py
@@ -63,6 +63,42 @@ class CarTest(unittest.TestCase):
         car.go("M")
         self.assertEqual(car.x, 0)
 
+# Partitionare de echivalenta
+# 1. Test pentru a verifica coordonate valide
+    def test_valid_coordinates(self):
+        car = Car(3, 3, "N")
+        self.assertEqual(car.x, 3)
+        self.assertEqual(car.y, 3)
+
+# 2. Test pentru a verifica coordonate invalide
+    def test_invalid_coordinates(self):
+        with self.assertRaises(ValueError):
+            Car(-1, 3, "N")
+        with self.assertRaises(ValueError):
+            Car(3, 8, "N")
+
+# 3. Test pentru a verifica directie valida
+    def test_valid_direction(self):
+        car = Car(3, 3, "N")
+        self.assertEqual(car.facing, "N")
+
+# 4. Test pentru a verifica directie invalida
+    def test_invalid_direction(self):
+        with self.assertRaises(ValueError):
+            Car(3, 3, "X")
+
+# 5. Test pentru verifica instructiune valida
+    def test_valid_instruction(self):
+            car = Car(3, 3, "N")
+            car.go("LR")
+            self.assertEqual(car.facing, "N")
+
+# 6. Test pentru verifica instructiune invalida
+    def test_invalid_instruction(self):
+        with self.assertRaises(ValueError):
+            car = Car(3, 3, "N")
+            car.go("X") 
+
 if __name__ == '__main__':
     car = Car(1, 1, "N")
     car.print()
@@ -71,4 +107,5 @@ if __name__ == '__main__':
     car.print()
     car.go("RMRMMMRMMM")
     car.print()
+
     unittest.main()
